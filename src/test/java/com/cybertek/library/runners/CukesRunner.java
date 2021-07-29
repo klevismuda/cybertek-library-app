8254bfbca65deeca8030251ec5e4bc5ec458de31
Klevis Muda <klevis.blue@gmail.com>
2021-07-29 10:19:31 +0200

    commit 2

--- src/test/java/com/cybertek/library/runners/CukesRunner.java
+++ src/test/java/com/cybertek/library/runners/CukesRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"html:target/cucumber-report.html",
                 "json:target/cucumber.json",
+                "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/cybertek/library/step_definitions",
